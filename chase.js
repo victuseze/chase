a44e1a7a01d296bae2bdded6cc5d0fa9fcbe2b73
victuseze <106381679+victuseze@users.noreply.github.com>
2023-05-14 22:53:18 +0100

    Add files via upload

---
+++ chase.js
@@ -0,0 +1,21 @@
+// let signin = document.getElementById('signin')
+
+// let alerting = (e)=>{
+//     e.preventDefault()
+//     incorrect.innerHTML = 'incorrect username/password'
+//     attempt.innerHTML = '1 more attempt left'
+//     incorrect.style.color = 'red'
+//     attempt.style.color = 'red'
+// }
+
+// document.getElementById('openNewTab').addEventListener('click', function() {
+//     var newWindow = window.open('loaded.html');
+//     var preloader = document.createElement('div');
+//     preloader.id = 'preloader';
+//     preloader.innerHTML = '<img src="https://media.giphy.com/media/xTk9ZvMnbIiIew7IpW/giphy.gif" alt="Loading...">';
+//     newWindow.document.body.appendChild(preloader);
+//     setTimeout(function() {
+//         newWindow.document.getElementById('preloader').style.display = 'none';
+//     }, 6000);
+// });
+
